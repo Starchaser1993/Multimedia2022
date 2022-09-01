9c6705bfce8d544107cfca5ae6744c5be5c0a00b
Starchaser1993 <97558693+Starchaser1993@users.noreply.github.com>
2022-09-01 14:09:02 -0400

    Clase 01-09-2022

---
+++ 01-09-2022 Condicionales.php
@@ -0,0 +1,41 @@
+<?php
+
+// Condiciones
+
+$numero_1 =4;
+$numero_2 =7;
+
+if ($numero_1 < $numero_2) {
+    echo "$numero_1 es menor que $numero_2";
+
+
+}elseif ($numero_1 == $numero_2) {
+    echo "$numero_1 y $numero_2 son iguales";
+
+}else {
+    echo "No se cumplen las condiciones";
+}
+//opcion 1
+if (condition) {
+    
+}
+//opcion 2
+if (condition) {
+    # code...
+} else {
+    # code...
+}
+//opcion 3
+if (condition) {
+    # code...
+}elseif (condition) {
+    # code...
+}
+//opcion 4
+if (condition) {
+    # code...
+}elseif (condition) {
+    # code...
+}elseif (condition) {
+    # code...
+}
---
+++ Operadores Logicos.php
@@ -0,0 +1,17 @@
+<?php
+
+// AND && son lo mismo
+var_dump(true and true);
+echo"<br>";
+var_dump(true and false);
+echo"<br>";
+var_dump(false && false);
+echo"<br>";
+
+// OR ||
+var_dump(true or false);
+var_dump(true || false);
+
+// Not !
+var_dump(!false);
+echo "<br>";
